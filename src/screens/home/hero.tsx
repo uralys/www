57845db88e1ebccda56d8cb5f76fed674cf895e6
Chris <chris.dugne@uralys.com>
2023-04-01 23:51:42 +0200

    added Logo :art:

--- src/screens/home/hero.tsx
+++ src/screens/home/hero.tsx
@@ -1,17 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
+import Logo from '../../../assets/images/logo.svg';
 
 // -----------------------------------------------------------------------------
 
 const $Hero = styled.div`
-  background-color: '#82d';
+  > h1 {
+    font-size: 3rem;
+    margin: 0;
+  }
+
+  > hr {
+    border: 0;
+  }
+
+  > hr:before {
+    content: '';
+    border: 0;
+    width: 3rem;
+    border-top: solid 2px #ffffff;
+    height: 2px;
+    display: inline-block;
+  }
 `;
 
 // -----------------------------------------------------------------------------
 
 const Hero = () => (
   <$Hero>
-    <h1>hero</h1>
+    <img src={Logo} width="120px" />
+    <h1>Uralys</h1>
+    <hr />
   </$Hero>
 );
 
