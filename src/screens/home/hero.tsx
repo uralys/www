9ddab9f0a3984e75c122cfedf98e00d89120bf95
Chris <chris.dugne@uralys.com>
2025-01-05 13:53:50 +0100

    using hello@uralys.com

--- src/screens/home/hero.tsx
+++ src/screens/home/hero.tsx
@@ -54,8 +54,8 @@ const Hero = () => (
     </p>
     <hr />
     <p>
-      <a href="mailto:hello@uralys.com">Let's talk</a>
-      <b>about your project.</b>
+      Let's talk about your project:{' '}
+      <a href="mailto:hello@uralys.com">hello@uralys.com</a>
     </p>
     <p>
       Here is my <a href="./cv-2024.pdf">CV</a>
